204d62277260a10d746dd378b2ca65bdff9c7b35
yortz <skylarking420@gmail.com>
2012-12-28 16:05:58 +0100

    pricing_rules accepts several variations of the same pattern "buy_X_get_X_free" via method_messing.

--- lib/ecommerce/checkout.rb
+++ lib/ecommerce/checkout.rb
@@ -1,10 +1,10 @@
 module Ecommerce
   class Checkout
-    attr_accessor :rule, :cart, :total
+    attr_accessor :pricing_rules, :cart, :total
 
-    def initialize(rule)
-      @rule = rule
-      @cart = []
+    def initialize(pricing_rules)
+      @pricing_rules = Array.new.push pricing_rules
+      @cart  = []
       @total = 0
     end
 
@@ -13,7 +13,53 @@ module Ecommerce
     end
 
     def total
-      @total = @cart.map(&:price).reduce(:+)
+      cart_total
+      calculate_discount_for @pricing_rules
+      get_total
     end
+
+   def method_missing(method_name, *args)
+     if method_name.to_s.split("_")[0] == "buy" && method_name.to_s.split("_").last == "free"
+       treshold = method_name.to_s.split("_")[1]
+       quantity = method_name.to_s.split("_")[3]
+       if treshold.match(/\d/) && quantity.match(/\d/)
+         discount(treshold, quantity, args.first)
+       end
+     else
+       super
+     end
+   end 
+
+   private
+
+   def get_total
+     @total - @discount
+   end
+
+   def cart_total
+      @total = @cart.map(&:price).reduce(:+)
+   end
+
+   def calculate_discount_for pricing_rules
+      pricing_rules.each do |rule|
+        send rule.split.first.to_sym, rule.split.last
+      end
+   end
+
+   def discount(treshold, quantity, item_code)
+     if code_for?(item_code) && quantity_for(item_code) == treshold.to_i
+       price    = Item.find_by_code(item_code).price
+       @discount = quantity.to_f * price
+     end
+   end
+
+  def code_for? item_code
+    @cart.map(&:code).include? "#{item_code}"
+  end
+
+  def quantity_for item_code
+    @cart.count {|item| item.code == "#{item_code}"} - 1 
+  end
+
   end
 end
--- spec/lib/ecommerce/checkout_spec.rb
+++ spec/lib/ecommerce/checkout_spec.rb
@@ -3,20 +3,20 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 describe "Checkout" do
 
-  let(:co) { Ecommerce::Checkout.new "FR1, SR1, FR1, CF1" }
+  let(:pricing_rules) { "buy_1_get_1_free FR1" }
+  let(:co) { Ecommerce::Checkout.new pricing_rules }
 
   describe "#init" do
     it "returns a set of rules" do
-      co.rule.should eq "FR1, SR1, FR1, CF1" 
+      co.pricing_rules.should eq ["buy_1_get_1_free FR1"]
     end
   end
 
+  let(:fr1) { Item.create! :code => "FR1", :name => "Fruit Tea", :price => "43.11" }
   describe "#scan" do
-
-    let(:item) { Item.new "FR1", "Fruit Tea", "£ 43.11" }
     
     it "adds an item to the cart" do
-      co.scan item
+      co.scan fr1
       cart_item = co.cart.first
       cart_item.code.should  eq "FR1"
       cart_item.name.should  eq "Fruit Tea"
@@ -24,9 +24,17 @@ describe "Checkout" do
     end
 
     it "calculates total from cart items" do
-      co.scan item
-      co.scan item
-      co.total.should eq 86.22
+      co.scan fr1
+      co.scan fr1
+      #co.total.should eq 86.22 need to edit this since we are now applying discount
+    end
+  end
+
+  describe "buy_1_get_1_free \"FR1\"" do
+    it "applies discount" do
+      co.scan fr1
+      co.scan fr1
+      co.total.should eq 43.11
     end
   end
   
